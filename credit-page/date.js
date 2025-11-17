a51dd731e95ac8de443ca94d0c6e3fabfc7003d5
Lucie-Pianelli <lucie.pianelli@etu.univ-amu.fr>
2025-11-17 16:34:50 +0100

    page credit (sans heure et sans css)

---
+++ credit-page/date.js
@@ -0,0 +1,6 @@
+/* Source - https://stackoverflow.com/a
+// Posted by Ullas, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-17, License - CC BY-SA 3.0*/
+
+var today = new Date();
+document.getElementById('time').innerHTML = today;
