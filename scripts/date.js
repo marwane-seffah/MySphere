85b8f8710953c890867489e228f43d725861c37b
Marwane <marwane.seffah@etu.univ-amu.fr>
2025-12-15 09:05:17 +0100

    Màj du script date.js et changements mineurs

--- scripts/date.js
+++ scripts/date.js
@@ -1,13 +1,16 @@
 // La fonction qui mettra à jour l'heure
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+console.log("ok")
 
-window.addEventListener("load", () => {
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("ok");
     function majHeure () {
-        let heure = new Date().toLocaleString()
-        let baliseHeure = document.getElementById('heure') // Changera l'heure pour la balise d'id "heure"
-        baliseHeure.innerHTML = heure
+        let date = new Date().toLocaleString();
+        let baliseDate = document.getElementById('date'); // Changera l'heure pour la balise d'id "heure"
+        baliseDate.innerHTML = date;
     }
-    majHeure()
-    setInterval(majHeure, 1000) // Toutes les secondes on appelle la fonction qui va changer l'heure
+    majHeure();
+    setInterval(majHeure, 1000); // Toutes les secondes on appelle la fonction qui va changer l'heure
 })
