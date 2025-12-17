831089a2031eea48d879cb381b4b6c222aef172d
Marwane <marwane.seffah@etu.univ-amu.fr>
2025-12-17 18:52:20 +0100

    résolution de quelques pblms sur les boutons et le style

---
+++ scripts/page.js
@@ -0,0 +1,19 @@
+function showWindowNewPost () {
+
+}
+
+function showDiscover () {
+    window.location.href='https://www.mickael-martin-nevot.com/'
+}
+
+function showFriends () {
+
+}
+
+function showSurprise () {
+    window.location.href='assets/y2k/rick_test_2.mp4'
+}
+
+function showWindowWriteSomething () {
+
+}
